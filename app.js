848ca1b8e1618870f9f9af791f368ed82602c7bf
prasadvakada007 <prasadvakada2@gmail.com>
2025-08-31 18:03:30 +0530

    add listen

--- app.js
+++ app.js
@@ -2,4 +2,4 @@
 const express = require('express');
 const app = express();
 app.get('/', (_req, res) => res.send('Hello from Node + Docker + Jenkins!'));
-app.listen(3000, () => console.log('App running on http://localhost:3000'));
+app.listen(3000, "0.0.0.0",() => console.log('App running on http://localhost:3000'));
